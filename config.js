04e7624c1f7b4d801652643f0ad37529349fe3d9
dawens-boy2 <github00p@gmail.com>
2025-05-30 11:44:48 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,79 +4,43 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id here",
-// add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DAWENS-XD 🖤*",
-// set the auto reply message on status reply  
-WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone mention you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/pbamxw.jpeg",
-// add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
-// add your prefix for bot   
-BOT_NAME: process.env.BOT_NAME || "𝐃𝐀𝐖𝐄𝐍𝐒-𝐗𝐃",
-// add bot name here for menu
-STICKER_NAME: process.env.STICKER_NAME || "𝐃𝐀𝐖𝐄𝐍𝐒-𝐗𝐃",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custom emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// choose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links without removing member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50942241547",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DAWENS",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀᴡᴇɴꜱ ᴛᴇᴀᴍ*",
-// add bot description    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/pbamxw.jpeg",
-// add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *𝐃𝐀𝐖𝐄𝐍𝐒-𝐗𝐃* ⚡",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-// make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-// make true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-// make false if want private mode
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "50942241547",
-// replace with your WhatsApp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", 
-// change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
-// make it true for auto recording 
+  SESSION_ID: process.env.SESSION_ID || "put your session id here",
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY ZaryaBot-V1 🖤*",
+  WELCOME: process.env.WELCOME || "true",
+  ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+  ANTI_LINK: process.env.ANTI_LINK || "true",
+  MENTION_REPLY: process.env.MENTION_REPLY || "false",
+  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/pbamxw.jpeg",
+  PREFIX: process.env.PREFIX || ".",
+  BOT_NAME: process.env.BOT_NAME || "𝐙𝐀𝐑𝐘𝐀𝐁𝐎𝐓-𝐕𝟏",
+  STICKER_NAME: process.env.STICKER_NAME || "𝐙𝐀𝐑𝐘𝐀𝐁𝐎𝐓-𝐕𝟏",
+  CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+  CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+  DELETE_LINKS: process.env.DELETE_LINKS || "false",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "50942241547",
+  OWNER_NAME: process.env.OWNER_NAME || "DAWENS",
+  DESCRIPTION: process.env.DESCRIPTION || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴢᴀʀʏᴀ ᴛᴇᴀᴍ*",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/pbamxw.jpeg",
+  LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *𝐙𝐀𝐑𝐘𝐀𝐁𝐎𝐓-𝐕𝟏* ⚡",
+  READ_MESSAGE: process.env.READ_MESSAGE || "false",
+  AUTO_REACT: process.env.AUTO_REACT || "false",
+  ANTI_BAD: process.env.ANTI_BAD || "false",
+  MODE: process.env.MODE || "public",
+  ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+  AUTO_VOICE: process.env.AUTO_VOICE || "false",
+  AUTO_STICKER: process.env.AUTO_STICKER || "false",
+  AUTO_REPLY: process.env.AUTO_REPLY || "false",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+  PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+  AUTO_TYPING: process.env.AUTO_TYPING || "false",
+  READ_CMD: process.env.READ_CMD || "false",
+  DEV: process.env.DEV || "50942241547",
+  ANTI_VV: process.env.ANTI_VV || "true",
+  ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same",
+  AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 };
