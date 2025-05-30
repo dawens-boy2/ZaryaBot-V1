b133df333b0d8206c40e5277fd508352ef07d21a
dawens-boy2 <github00p@gmail.com>
2025-05-30 14:33:41 -0400

    Update ytmp4.js

--- plugins/ytmp4.js
+++ plugins/ytmp4.js
@@ -50,7 +50,7 @@ cmd({
             `🎞 *Reply with your choice:*\n` +
             `2.1 *Video Type* 🎬\n` +
             `2.2 *Document Type* 📁\n\n` +
-            `${config.FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ dawens*"}`;
+            `${config.FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐙𝐚𝐫𝐲𝐚𝐁𝐨𝐭-𝐕𝟏*"}`;
 
         const sentMsg = await conn.sendMessage(from, { image: { url: image }, caption: info }, { quoted: mek });
         const messageID = sentMsg.key.id;
